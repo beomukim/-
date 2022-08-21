0e410acca86cd9214d89fd6969d698afa8128bdb
bumwoo <gld55602@gmail.com>
2022-08-21 21:15:00 +0900

    계산기 프로그램 웹 어플리케이션으로 만들기

--- src/test/java/org/example/calculator/CalculatorTest.java
+++ src/test/java/org/example/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
-package org.example;
+package org.example.calculator;
 
+import org.example.calculator.Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
